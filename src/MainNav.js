5cae08c5aeea852fc39b8828bf5ce5356bb6c6fa
programarivm <info@programarivm.com>
2022-02-12 18:23:02 +0100

    Fixed matched leaf route at location / does not have an element

--- src/MainNav.js
+++ src/MainNav.js
@@ -1,9 +1,6 @@
-import { AppBar, Button, Grid, Toolbar } from '@mui/material';
+import { AppBar, Grid, Toolbar } from '@mui/material';
 import { makeStyles } from '@mui/styles';
-import { Link, Route, Routes } from 'react-router-dom';
-import InfoIcon from '@mui/icons-material/Info';
 import { Chess } from '@chesslablab/redux-chess';
-import About from "./About.js";
 import logo from './assets/img/logo.png';
 
 const useStyles = makeStyles({
@@ -21,43 +18,20 @@ const MainNav = () => {
         <Toolbar>
           <Grid container justifyContent="space-between">
             <Grid item>
-              <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-                <img src={logo} className="logo" alt="logo" />
-              </Link>
-            </Grid>
-            <Grid item style={{ marginTop: 10 }}>
-              <Link to="/about" style={{ textDecoration: 'none', color: '#505050'}}>
-                <Button
-                  variant="text"
-                  color="inherit"
-                  style={{textTransform: 'none'}}
-                  startIcon={<InfoIcon />}
-                >
-                  About
-                </Button>
-              </Link>
+              <img src={logo} className="logo" alt="logo" />
             </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
-      <Routes>
-        <Route
-          exact path="/"
-          render={(props) => <div className={classes.chess}>
-            <Chess props={{
-              server: {
-                prot: 'wss',
-                host: 'pchess.net',
-                port: '8443'
-              }
-            }} />
-          </div>}
-        />
-        <Route
-          path="/about"
-          render={(props) => <About {...props} />}
-        />
-      </Routes>
+      <div className={classes.chess}>
+        <Chess props={{
+          server: {
+            prot: 'wss',
+            host: 'pchess.net',
+            port: '8443'
+          }
+        }} />
+      </div>
     </div>
   );
 }
