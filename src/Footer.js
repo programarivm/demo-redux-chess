a7c56457357e8dcb70c40725a3b3dddedc9dbfae
programarivm <info@programarivm.com>
2022-05-17 16:38:12 +0200

    Updated the main navigation

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Grid, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import Typography from '@mui/material/Typography';
 import CookieConsent from 'react-cookie-consent';
+import cube from './assets/img/cube.png';
 
 const Footer = () => {
   const { pathname } = useLocation();
@@ -18,13 +27,19 @@ const Footer = () => {
     <>
       <Grid container style={{ marginTop: 50, borderTop: '1px solid #ccc', padding: 30 }}>
         <Grid item xs={12} sm={12} md={1} />
-        <Grid item xs={12} sm={12} md={5}>
-          <Typography color="secondary" variant="subtitle2">
-            ChesslabLab,<br/>
-            Open Source community<br/>
-            Happy learning and coding!
-          </Typography>
+        <Grid item xs={12} sm={12} md={4}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                ChesslabLab
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Open Source community to learn web development and chess. Happy learning and coding!
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
+        <Grid item xs={12} sm={12} md={1} />
         <Grid item xs={12} sm={12} md={3}>
           <List component="nav" aria-label="social">
             <a href="https://github.com/chesslablab" style={{ textDecoration: 'none' }}>
@@ -33,14 +48,6 @@ const Footer = () => {
                   <GitHubIcon />
                 </ListItemIcon>
                 <ListItemText secondary="GitHub" />
-              </ListItem>
-            </a>
-            <a href="https://programarivm.medium.com" style={{ textDecoration: 'none' }}>
-              <ListItem button>
-                <ListItemIcon>
-                  <MenuBookIcon />
-                </ListItemIcon>
-                <ListItemText secondary="Medium" />
               </ListItem>
             </a>
             <a href="https://twitter.com/programarivm" style={{ textDecoration: 'none' }}>
--- src/MainNav.js
+++ src/MainNav.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { AppBar, Button, CssBaseline, Grid, Toolbar } from '@mui/material';
 import InfoIcon from '@mui/icons-material/Info';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 import logo from './assets/img/logo.png';
 
 const MainNav = () => {
@@ -16,7 +17,7 @@ const MainNav = () => {
               </Link>
             </Grid>
           </Grid>
-          <Link to="/about" style={{ textDecoration: 'none', color: '#505050'}}>
+          <Link to="/about" style={{ textDecoration: 'none', color: '#505050', marginRight: 15}}>
             <Button
               variant="text"
               color="inherit"
@@ -26,6 +27,16 @@ const MainNav = () => {
               About
             </Button>
           </Link>
+          <a href="https://programarivm.medium.com" style={{ textDecoration: 'none', color: '#505050'}}>
+            <Button
+              variant="text"
+              color="inherit"
+              style={{textTransform: 'none'}}
+              startIcon={<MenuBookIcon />}
+            >
+              Blog
+            </Button>
+          </a>
         </Toolbar>
       </AppBar>
     </>
